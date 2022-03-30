8fbc58dbe5eca46146f01144dfe1312a0fb383f4
Mejia <jmejia@FVFZMY3WJ1WK.tld>
2022-03-30 10:53:41 -0500

    work from march 29

---
+++ app.js
@@ -0,0 +1,11 @@
+function askName() {
+    var n = prompt("Enter your name:" )
+    return n
+  }
+
+function RanNum() {
+   var gen = Math.floor(Math.random() * 10) +1
+
+   var gen1 = Math.floor(Math.random() * 10) +1
+}
+RanNum(askName())
